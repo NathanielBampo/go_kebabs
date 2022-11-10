eb9f5bff36cca3ccf7a16d0ad1f88db7d6f0590b
NathanielBampo <mergontv@gmail.com>
2022-11-10 12:23:31 +0400

    Deploy Render Final

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV['RENDER'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   # config.action_mailer.delivery_method = :mailjet
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.default_url_options = { host: 'copbygp.ru' }
+  config.action_mailer.default_url_options = { host: 'kebabs.onrender.com' }
   # config.action_mailer.raise_delivery_errors = false
   # config.action_mailer.perform_deliveries = true
   # config.action_mailer.delivery_method = :mailjet
